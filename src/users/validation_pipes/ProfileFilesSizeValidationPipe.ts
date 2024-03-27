5b36c1b8b5cc15b1447becb4e6fa6a8a391a4de6
Charlotte-Overeem <r0781396@student.thomasmore.be>
2024-03-27 14:01:49 +0100

    changed: returned error messages when file validation fails

--- src/users/validation_pipes/ProfileFilesSizeValidationPipe.ts
+++ src/users/validation_pipes/ProfileFilesSizeValidationPipe.ts
@@ -6,10 +6,16 @@ export class ProfileFilesSizeValidationPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
     const maxSize = 500000; // 500 kb
     if (value.profile_picture[0].size > maxSize) {
-      return "Error: size of " + value.profile_picture[0].fieldname + " exceeds the maximum file size (" + maxSize + ")";
+      return {
+        "error_type": "Validation failed",
+        "error_message": "Error: size of " + value.profile_picture[0].fieldname + " exceeds the maximum file size (" + maxSize + ")"
+      };
     }
     if (value.banner_picture[0].size > maxSize) {
-      return "Error: size of " + value.banner_picture[0].fieldname + " exceeds the maximum file size (" + maxSize + ")";
+      return {
+        "error_type": "Validation failed",
+        "error_message": "Error: size of " + value.banner_picture[0].fieldname + " exceeds the maximum file size (" + maxSize + ")"
+      };
     }
 
     return value;
--- src/users/validation_pipes/ProfileFilesTypesValidationPipe.ts
+++ src/users/validation_pipes/ProfileFilesTypesValidationPipe.ts
@@ -7,10 +7,16 @@ export class ProfileFilesTypesValidationPipe implements PipeTransform {
     const fileTypes = ["image/png", "image/jpg", "image/jpeg"]; // 500 kb
 
     if (!fileTypes.includes(value.profile_picture[0].mimetype)) {
-      return "Error: the file type of " + value.profile_picture[0].fieldname + " is not an accepted file type for this upload (" + fileTypes + ")";
+      return {
+        "error_type": "Validation failed",
+        "error_message": "Error: the file type of " + value.profile_picture[0].fieldname + " is not an accepted file type for this upload (" + fileTypes + ")"
+      };
     }
     if (!fileTypes.includes(value.banner_picture[0].mimetype)) {
-      return "Error: the file type of " + value.banner_picture[0].fieldname + " is not an accepted file type for this upload (" + fileTypes + ")";
+      return {
+        "error_type": "Validation failed",
+        "error_message": "Error: the file type of " + value.banner_picture[0].fieldname + " is not an accepted file type for this upload (" + fileTypes + ")"
+      };
     }
 
     return value;
