0d455768cdfe0605105594a23707e353311950b1
Dylan <79404455+DylanBracke@users.noreply.github.com>
2024-05-27 11:39:30 +0200

    error handling feedbackquestions

--- src/feedback_questions/feedback_questions.controller.ts
+++ src/feedback_questions/feedback_questions.controller.ts
@@ -1,34 +1,85 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpException,
+  HttpStatus,
+  NotFoundException,
+} from '@nestjs/common';
 import { FeedbackQuestionsService } from './feedback_questions.service';
 import { CreateFeedbackQuestionDto } from './dto/create-feedback_question.dto';
 import { UpdateFeedbackQuestionDto } from './dto/update-feedback_question.dto';
 
 @Controller('feedback-questions')
 export class FeedbackQuestionsController {
-  constructor(private readonly feedbackQuestionsService: FeedbackQuestionsService) {}
+  constructor(
+    private readonly feedbackQuestionsService: FeedbackQuestionsService,
+  ) {}
 
   @Post()
-  create(@Body() createFeedbackQuestionDto: CreateFeedbackQuestionDto) {
-    return this.feedbackQuestionsService.createQuestion(createFeedbackQuestionDto);
+  async create(@Body() createFeedbackQuestionDto: CreateFeedbackQuestionDto) {
+    try {
+      return await this.feedbackQuestionsService.createQuestion(
+        createFeedbackQuestionDto,
+      );
+    } catch (error) {
+      console.log(error);
+      return new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Get()
-  findAll() {
-    return this.feedbackQuestionsService.findAllQuestions();
+  async findAll() {
+    try {
+      return await this.feedbackQuestionsService.findAllQuestions();
+    } catch (error) {
+      console.log(error);
+      return new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.feedbackQuestionsService.findOneQuestion(id);
+  async findOne(@Param('id') id: string) {
+    try {
+      return await this.feedbackQuestionsService.findOneQuestion(id);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        return new HttpException(error.message, HttpStatus.NOT_FOUND);
+      }
+      console.log(error);
+      return new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFeedbackQuestionDto: UpdateFeedbackQuestionDto) {
-    return this.feedbackQuestionsService.updateQuestion(id, updateFeedbackQuestionDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateFeedbackQuestionDto: UpdateFeedbackQuestionDto,
+  ) {
+    try {
+      return await this.feedbackQuestionsService.updateQuestion(
+        id,
+        updateFeedbackQuestionDto,
+      );
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        return new HttpException(error.message, HttpStatus.NOT_FOUND);
+      } else {
+        console.log(error);
+        return new HttpException(
+          error.message,
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.feedbackQuestionsService.removeQuestion(id);
+  async remove(@Param('id') id: string) {
+    return await this.feedbackQuestionsService.removeQuestion(id);
   }
 }
--- src/feedback_questions/feedback_questions.service.ts
+++ src/feedback_questions/feedback_questions.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateFeedbackQuestionDto } from './dto/create-feedback_question.dto';
 import { UpdateFeedbackQuestionDto } from './dto/update-feedback_question.dto';
 import { FeedbackQuestion } from './entities/feedback_question.entity';
@@ -12,56 +12,86 @@ export class FeedbackQuestionsService {
     private readonly questionsRepository: Repository<FeedbackQuestion>,
   ) {}
 
-
-
-  /** 
- * @throws {Error}
- */
+  /**
+   * @throws {Error}
+   */
   createQuestion(createFeedbackQuestionDto: CreateFeedbackQuestionDto) {
-    try{
-    this.questionsRepository.save(createFeedbackQuestionDto);
-    return createFeedbackQuestionDto;
-    }catch(err){
+    try {
+      this.questionsRepository.save(createFeedbackQuestionDto);
+      return createFeedbackQuestionDto;
+    } catch (err) {
       throw new Error(err);
     }
   }
 
-    /** 
- * @throws {Error}
- */
+  /**
+   * @throws {Error}
+   */
   findAllQuestions() {
-    try{
-    return this.questionsRepository.find();
-    }catch(err){
+    try {
+      return this.questionsRepository.find();
+    } catch (err) {
       throw new Error(err);
     }
   }
 
-  
-  findOneQuestion(id: string) {
-    try{
-    return this.questionsRepository.findOneBy({ id });
-  }catch(err){
-    throw new Error(err);
-  }
+  /**
+   * @async
+   * @param id - string
+   * @returns Promise<FeedbackQuestion[]>
+   * @throws {Error | NotFoundException}
+   */
+  async findOneQuestion(id: string) {
+    try {
+      const question: FeedbackQuestion =
+        await this.questionsRepository.findOneBy({ id });
+      if (!question) {
+        throw new NotFoundException(`Question with id ${id} not found.`);
+      }
+      return question;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException(error.message);
+      } else {
+        throw new Error(`Error while looking up FeedbackQuestion ${id} :` + error.message);
+      }
+    }
   }
 
-  async updateQuestion(id: string, updateFeedbackQuestionDto: UpdateFeedbackQuestionDto) {
-    try{
-    let updatequestion: FeedbackQuestion = await this.questionsRepository.findOneBy({ id });
-    updatequestion.question_title = updateFeedbackQuestionDto.question_title;
-    this.questionsRepository.save(updatequestion);
-    return updatequestion;
-  }catch(err){
-    throw new Error(err);
-  }
+
+  /**
+   * @async
+   * @param id - string
+   * @param updateFeedbackQuestionDto
+   * @returns Promise<FeedbackQuestion>
+   * @throws {Error | NotFoundException}
+   */
+  async updateQuestion(
+    id: string,
+    updateFeedbackQuestionDto: UpdateFeedbackQuestionDto,
+  ) {
+    try {
+      let updatequestion: FeedbackQuestion =
+        await this.questionsRepository.findOneBy({ id });
+      if (!updatequestion) {
+        throw new NotFoundException(`Question with id ${id} not found.`);
+      }
+      updatequestion.question_title = updateFeedbackQuestionDto.question_title;
+      this.questionsRepository.save(updatequestion);
+      return updatequestion;
+    } catch (err) {
+      if (err instanceof NotFoundException) {
+        throw new NotFoundException(err.message);
+      }
+      throw new Error(err);
+    }
   }
 
   async removeQuestion(id: string) {
-    try{
-    return await this.questionsRepository.delete(id);
-  }catch(err){
-    throw new Error(err);
-  }
+    try {
+      return await this.questionsRepository.delete(id);
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 }
